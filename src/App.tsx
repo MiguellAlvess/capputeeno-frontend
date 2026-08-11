1646d71baa8225941d3e0d86b3678a8d5aad6de9
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-08-11 13:54:14 -0300

    chore: add tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => {
-  return <h1>Teste</h1>
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-3xl font-bold text-red-500 underline">Teste</h1>
+        <h1 className="text-3xl font-bold text-red-500 underline"> Teste</h1>
+      </div>
+    </>
+  )
 }
 
 export default App
