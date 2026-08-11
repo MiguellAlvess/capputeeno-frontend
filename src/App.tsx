ca2301867b6058f11e3be43e7fb05dda120f4cfc
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-08-11 14:01:22 -0300

    chore: add shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from './components/ui/button'
+
 const App = () => {
   return (
     <>
       <div className="flex h-screen items-center justify-center">
         <h1 className="text-3xl font-bold text-red-500 underline">Teste</h1>
         <h1 className="text-3xl font-bold text-red-500 underline"> Teste</h1>
+        <Button>Teste</Button>
       </div>
     </>
   )
