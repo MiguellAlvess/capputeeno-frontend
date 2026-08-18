2df17eb3cfcb127e11e8c4810ca80862079cff99
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-08-18 15:16:29 -0300

    chore: configure mocked API with MSW

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,18 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import { AppRoutes } from './app/routes/routes'
+import { worker } from './mocks/browser'
 
 const queryClient = new QueryClient()
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </QueryClientProvider>
-  </StrictMode>
-)
+worker.start().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </StrictMode>
+  )
+})
