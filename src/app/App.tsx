9fc2a63817d66b7d156868200d661d58efc00c20
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-08-18 14:06:01 -0300

    refactor: reorganize folder structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from './components/ui/button'
-
 const App = () => {
   return (
     <>
       <div className="flex h-screen items-center justify-center">
         <h1 className="text-3xl font-bold text-red-500 underline">Teste</h1>
         <h1 className="text-3xl font-bold text-red-500 underline"> Teste</h1>
-        <Button>Teste</Button>
       </div>
     </>
   )
