4f83ac9d494466ea3ca6e78fe660b4f3dc2580bd
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-08-31 10:48:20 -0300

    feat: add product catalog route

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,12 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 
+import { ProductCatalogPage } from '@/catalog'
+
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>Catálogo</div>} />
-      <Route path="/products/:productId" element={<div>Produto</div>} />
-      <Route path="/cart" element={<div>Carrinho</div>} />
-      <Route path="/admin/products/new" element={<div>Novo produto</div>} />
+      <Route path="/" element={<ProductCatalogPage />} />
     </Routes>
   )
 }
